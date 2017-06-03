17a144dfae61573a7322f32204db56e6982195ea
Filip Wieland <filiplamparski@gmail.com>
2017-06-03 18:39:37 +0100

    Note editor component

---
+++ src/components/note-editor/index.js
@@ -0,0 +1,21 @@
+import {h, Component} from 'preact'; // eslint-disable-line no-unused-vars
+import {connect} from 'preact-redux';
+import {Button} from 'react-toolbox/lib/button';
+
+import style from './style';
+
+import {saveNote} from 'actions/note';
+import {noteEditorTextChanged, noteEditorClose} from 'actions/note-editor';
+
+function NoteEditor({dispatch, noteId, text}) {
+  const actionsOff = !noteId && text === '';
+  return (
+    <div class={style.container}>
+      <textarea placeholder="Note text..." class={style.entry} oninput={e => dispatch(noteEditorTextChanged(e.target.value))} value={text}></textarea>
+      <Button disabled={actionsOff} raised primary type="button" onclick={() => [saveNote({noteId, text}), noteEditorClose()].map(dispatch)}>Save</Button>
+      <Button disabled={actionsOff} type="button" onclick={() => dispatch(noteEditorClose())}>Cancel</Button>
+    </div>
+  );
+}
+
+export default connect(state => state.noteEditor)(NoteEditor);
--- src/components/note-list/index.js
+++ src/components/note-list/index.js
@@ -5,12 +5,13 @@ import style from './style';
 
 import Note from 'components/note';
 import {deleteNote} from 'actions/note';
+import {noteEditorOpen} from 'actions/note-editor';
 
 function NoteList({notes, dispatch}) {
 	if (Object.keys(notes).length > 0) {
 		return (
 			<main class={style.noteGrid}>
-				{Object.keys(notes).map(id => <Note note={notes[id]} onDelete={() => dispatch(deleteNote(id))} />)}
+				{Object.keys(notes).map(id => <Note note={notes[id]} onDelete={() => dispatch(deleteNote(id))} onEdit={() => dispatch(noteEditorOpen({noteId: id, text: notes[id].text}))} />)}
 			</main>
 		);
 	}

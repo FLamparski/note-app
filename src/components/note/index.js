0e24143d2dc8a669e0109c39f90d6b9aa0b74383
Filip Wieland <filiplamparski@gmail.com>
2017-06-03 18:41:59 +0100

    Add an edit button to notes

--- src/components/note/index.js
+++ src/components/note/index.js
@@ -3,12 +3,13 @@ import {h} from 'preact'; // eslint-disable-line no-unused-vars
 import {Button} from 'react-toolbox/lib/button';
 import {Card, CardTitle, CardText, CardActions} from 'react-toolbox/lib/card';
 import MaterialRemove from 'material-design-icons/content/svg/production/ic_clear_24px.svg';
+import MaterialPencil from 'material-design-icons/editor/svg/design/ic_mode_edit_24px.svg';
 
 import style from './style';
 
 import SvgIcon from 'components/svg-icon';
 
-export default function Note({note, onDelete}) {
+export default function Note({note, onDelete, onEdit}) {
 	return (
 		<Card className={style.note}>
 			<CardTitle
@@ -16,10 +17,11 @@ export default function Note({note, onDelete}) {
 				title="User"
 				subtitle={note.fromNow} />
 
-			<CardText>{note.text}</CardText>
+			<CardText className={style.noteInner}>{note.text}</CardText>
 
 			<CardActions>
 				<Button onClick={onDelete} className={style.noteButton} mini><SvgIcon icon={MaterialRemove} /> Delete</Button>
+				<Button onClick={onEdit} className={style.noteButton} mini><SvgIcon icon={MaterialPencil} /> Edit</Button>
 			</CardActions>
 		</Card>
 	);

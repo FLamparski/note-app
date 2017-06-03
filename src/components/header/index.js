f10958b4a9799d0b5953a570ae6d98902e87cea5
Filip Wieland <filiplamparski@gmail.com>
2017-06-03 18:45:06 +0100

    Formatting and linter fixes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,14 @@
 import {h} from 'preact'; // eslint-disable-line no-unused-vars
-import {connect} from 'preact-redux';
+// import {connect} from 'preact-redux';
 
 import {AppBar} from 'react-toolbox/lib/app_bar';
 
 import style from './style';
 
-function Header({dispatch}) {
+export default function Header() {
 	return (
     <AppBar title="NoteApp" className={style.header} />
 	);
 }
 
-export default connect(x => x)(Header);
+// Export default connect(x => x)(Header);
--- src/components/note-editor/index.js
+++ src/components/note-editor/index.js
@@ -8,14 +8,14 @@ import {saveNote} from 'actions/note';
 import {noteEditorTextChanged, noteEditorClose} from 'actions/note-editor';
 
 function NoteEditor({dispatch, noteId, text}) {
-  const actionsOff = !noteId && text === '';
-  return (
+	const actionsOff = !noteId && text === '';
+	return (
     <div class={style.container}>
       <textarea placeholder="Note text..." class={style.entry} oninput={e => dispatch(noteEditorTextChanged(e.target.value))} value={text}></textarea>
       <Button disabled={actionsOff} raised primary type="button" onclick={() => [saveNote({noteId, text}), noteEditorClose()].map(dispatch)}>Save</Button>
       <Button disabled={actionsOff} type="button" onclick={() => dispatch(noteEditorClose())}>Cancel</Button>
     </div>
-  );
+	);
 }
 
 export default connect(state => state.noteEditor)(NoteEditor);
